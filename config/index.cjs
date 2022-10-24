b09445d49f0f32dc36f68ed5c7de8742edbf2f8a
xuepenghui-ttt <56376126+xuepenghui-ttt@users.noreply.github.com>
2022-10-24 15:09:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oGDwU6aPAAOzmtkuB3DyZgRnrd-o',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '2KWQXxaDpzPiWVJ5uCb5eusVgS7bIx4Me4fF791XfvE',
+      useTemplateId: '93g7IrVm_0YYiMBBvhoxYxFGcOXLVQ-MUhU-Ht0memM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-09',
       festivals: [
@@ -33,7 +33,7 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1999', date: '04-09',
+          type: '生日', name: '宝贝', year: '1999', date: '04-09',
         },
         {
           type: '节日', name: '相识纪念日', year: '2015', date: '09-01',
@@ -42,14 +42,14 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2019-02-17' },
+        { keyword: 'love_day', date: '2019-02-16' },
       ],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '2KWQXxaDpzPiWVJ5uCb5eusVgS7bIx4Me4fF791XfvE',
+  CALLBACK_TEMPLATE_ID: '93g7IrVm_0YYiMBBvhoxYxFGcOXLVQ-MUhU-Ht0memM',
 
   CALLBACK_USERS: [
     {
