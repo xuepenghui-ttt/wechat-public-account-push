3334478d3601e2c2878adc3f3ba3fa04ffbda308
xuepenghui-ttt <56376126+xuepenghui-ttt@users.noreply.github.com>
2022-10-26 09:15:02 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -31,11 +31,11 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1999', date: '02-23',
+          type: '*阴历生日', name: '宝贝', year: '1999', date: '02-23',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '宝贝', year: '1999', date: '04-09',
+          type: '阳历生日', name: '宝贝', year: '1999', date: '04-09',
         },
         {
           type: '节日', name: '相识纪念日', year: '2015', date: '09-01',
