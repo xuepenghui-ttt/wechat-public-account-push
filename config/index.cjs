d49fb16e11b6fb85d1c8240e4761eb4ca58de1fc
xuepenghui-ttt <56376126+xuepenghui-ttt@users.noreply.github.com>
2023-05-15 16:01:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oGDwU6YFF8xt2RCPx1oVI-4f328s',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'iwMuM2LXrsxlcjXkGs8Lc1RTz_zEXZlsXsry0t2BtMA',
+      useTemplateId: 'cGGmr3S7HZTZkYZHkXVnO3hSXR7ADcmGJ-_xh90QAcQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-09',
       festivals: [
@@ -45,7 +45,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'cGGmr3S7HZTZkYZHkXVnO3hSXR7ADcmGJ-_xh90QAcQ',
+  CALLBACK_TEMPLATE_ID: '2Nm_ScInWj7jqR06WWwGBA309tLTfYZmrjjUg_W42iM',
 
   CALLBACK_USERS: [
     {
