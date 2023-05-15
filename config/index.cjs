15fcffadd3c4dc97e32dea6745bf27f29ad7a2ad
xuepenghui-ttt <56376126+xuepenghui-ttt@users.noreply.github.com>
2023-05-15 15:50:53 +0800

    推送模板

--- config/index.cjs
+++ config/index.cjs
@@ -45,7 +45,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '	cGGmr3S7HZTZkYZHkXVnO3hSXR7ADcmGJ-_xh90QAcQ',
+  CALLBACK_TEMPLATE_ID: 'cGGmr3S7HZTZkYZHkXVnO3hSXR7ADcmGJ-_xh90QAcQ',
 
   CALLBACK_USERS: [
     {
