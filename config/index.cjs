caddd6d8d7e4ae1a6b18c9da8ee9e64f6ded8bbc
xuepenghui-ttt <56376126+xuepenghui-ttt@users.noreply.github.com>
2023-05-26 09:29:26 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小田宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oGDwU6YFF8xt2RCPx1oVI-4f328s',
+      id: 'oGDwU6aPAAOzmtkuB3DyZgRnrd-o',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'cGGmr3S7HZTZkYZHkXVnO3hSXR7ADcmGJ-_xh90QAcQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
