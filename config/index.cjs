669703dcc1bf210407299b23e83cc5ad34c9b6c4
xuepenghui-ttt <56376126+xuepenghui-ttt@users.noreply.github.com>
2023-06-20 21:43:46 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,7 +19,7 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '小田宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oGDwU6aPAAOzmtkuB3DyZgRnrd-o',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
