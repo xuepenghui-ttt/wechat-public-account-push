6eec41f34b2beb69d5022d0573f498da68cc20f2
xuepenghui-ttt <56376126+xuepenghui-ttt@users.noreply.github.com>
2023-06-25 16:36:24 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,7 +22,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oGDwU6YFF8xt2RCPx1oVI-4f328s',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '-Ctk_NSOxA8w__7hh6UCcxbRsJrPEBHoIKaZcbOL7F0',
+      useTemplateId: 'CmF9rUnCKSU6TcUSdlkaxfTSIu0X9BQORGCEnVtlauY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '02-18',
     },
