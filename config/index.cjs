51794f7a52e63b7797678703f1c43cb1da0cff22
xuepenghui-ttt <56376126+xuepenghui-ttt@users.noreply.github.com>
2023-06-25 16:07:45 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '贝贝仙女',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oGDwU6XrulV9-WYknFIb_T8J3f28',
+      id: 'oGDwU6YFF8xt2RCPx1oVI-4f328s',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '-Ctk_NSOxA8w__7hh6UCcxbRsJrPEBHoIKaZcbOL7F0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
